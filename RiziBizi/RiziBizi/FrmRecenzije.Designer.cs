ef0b3deeb9e054c66b002bc909af6287680a3ac9
Stjepan Lovrić <slovric22@foi.hr>
2024-05-25 19:24:40 +0200

    Create class "Narudžbe".

--- RiziBizi/RiziBizi/FrmRecenzije.Designer.cs
+++ RiziBizi/RiziBizi/FrmRecenzije.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmRecenzije
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "FrmRecenzije";
+            this.Name = "FrmRecenzije";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Recenzije";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
---
+++ RiziBizi/RiziBizi/Models/Narudžbe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiziBizi.Models
+{
+    public abstract class Narudžbe
+    {
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public string Sastojci { get; set; }
+        public override string ToString()
+        {
+            return Naziv + " " + Sastojci;
+        }
+
+    }
+}
