136d6febedfd4b200ef9c85b09a56ac1e80a1873
Stjepan Lovrić <slovric22@foi.hr>
2024-05-25 19:26:08 +0200

    Create class Student.

---
+++ RiziBizi/RiziBizi/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiziBizi.Models
+{
+    public abstract class Student
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public override string ToString()
+        {
+            return Ime + " " + Prezime;
+        }
+
+    }
+}
